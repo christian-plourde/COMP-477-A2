4012ee7bfd0ca7b20759b0eb97fd5707503e988b
Christian Plourde <christian.bertin.plourde@gmail.com>
2019-11-09 17:34:01 -0500

    four link arm

--- main.cpp
+++ main.cpp
@@ -5,7 +5,6 @@
 #include <iostream>
 #include <vector>
 #include "include/cugl.h"
-#include "GLUT/GL/glut.h"
 
 using namespace std;
 using namespace cugl;
@@ -68,21 +67,25 @@ private:
 };
 
 // The arm has three components
-Link *upper;
-const double L = 25;
-double theta = -PI/4;
+Link* link_1;
+const double link_1_length = 25;
+double a_1 = -PI/4;
 
-Link *middle;
-const double M = 15;
-double phi = -PI/4;
+Link* link_2;
+const double link_2_length = 20;
+double a_2 = -PI/4;
 
-Link *lower;
-const double S = 10;
-double psi = -PI/4;
+Link* link_3;
+const double link_3_length = 15;
+double a_3 = -PI/4;
+
+Link* link_4;
+const double link_4_length = 10;
+double a_4 = -PI/4;
 
 // Target point - moved when the arm has reached it
-double tX = L;
-double tY = S;
+double tX = link_3_length;
+double tY = link_4_length;
 
 // Current position of tip
 double x;
@@ -91,27 +94,25 @@ double y;
 // Control step size
 double step = 0.01;
 
-// Points passed through on this trip
-vector<Point> points;
-
 // Choose a random target for the tip to aim at
 void chooseTarget()
 {
     double rad = 0, ang = 2 * PI * randReal();
-    rad = 2 + (L + M + S - 3) * randReal();
+    rad = 2 + (link_1_length + link_2_length + link_3_length + link_4_length - 3) * randReal();
     tX = rad * cos(ang);
     tY = rad * sin(ang);
-    points.clear();
 }
 
 // Initialize arm and target
 void initialize()
 {
-    upper = new Link(L, red);
-    middle = new Link(M, green);
-    lower = new Link(S, blue);
-    upper->addLink(middle);
-    middle->addLink(lower);
+    link_1 = new Link(link_1_length, red);
+    link_2 = new Link(link_2_length, green);
+    link_3 = new Link(link_3_length, blue);
+    link_4 = new Link(link_4_length, white);
+    link_1->addLink(link_2);
+    link_2->addLink(link_3);
+    link_3->addLink(link_4);
     chooseTarget();
 }
 
@@ -120,7 +121,7 @@ void display (void)
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
-    glTranslated(0, 0, -150);
+    glTranslated(0, 0, -200);
     glRotated(90, 0, 1, 0);
 
     // Show target
@@ -130,32 +131,25 @@ void display (void)
     glutSolidSphere(1, 20, 20);
     glPopMatrix();
 
-    // Record point and show trail
-    points.push_back(Point(0, -y, x));
-    glDisable(GL_LIGHTING);
-    glColor3d(1, 1, 1);
-    glBegin(GL_POINTS);
-    for (vector<Point>::const_iterator i = points.begin(); i != points.end(); ++i)
-        i->draw();
-    glEnd();
-    glEnable(GL_LIGHTING);
-
     // Draw robot arm
     glutSolidSphere(3, 20, 20);
-    upper->draw();
+    link_1->draw();
 
     glutSwapBuffers();
 }
 
 void idle()
 {
-    static double oldDTheta = 0;
-    static double oldDPhi = 0;
-    static double oldDPsi = 0;
+    static double oldDA1 = 0;
+    static double oldDA2 = 0;
+    static double oldDA3 = 0;
+    static double oldDA4 = 0;
 
     // Current position of tip
-    x = L * cos(theta) + M * cos(theta + phi) + S * cos(theta + phi + psi);
-    y = L * sin(theta) + M * sin(theta + phi) + S * sin(theta + phi + psi);
+    x = link_1_length * cos(a_1) + link_2_length * cos(a_1 + a_2) + link_3_length * cos(a_1 + a_2 + a_3)
+            + link_4_length * cos(a_1 + a_2 + a_3 + a_4);
+    y = link_1_length * sin(a_1) + link_2_length * sin(a_1 + a_2) + link_3_length * sin(a_1 + a_2 + a_3)
+        + link_4_length * sin(a_1 + a_2 + a_3 + a_4);
 
     // Position of tip relative to target
     double deltaX = tX - x;
@@ -175,26 +169,30 @@ void idle()
     deltaY *= rd;
 
     // Find partial derivatives
-    double dx_dpsi = - S * sin(theta + phi + psi);
-    double dx_dphi = - M * sin(theta + phi) + dx_dpsi;
-    double dx_dtht = - L * sin(theta) + dx_dphi;
+    double dx_da4 = - link_4_length * sin(a_1 + a_2 + a_3 + a_4);
+    double dx_da3 = - link_3_length * sin(a_1 + a_2 + a_3) + dx_da4;
+    double dx_da2 = - link_2_length * sin(a_1 + a_2) + dx_da3;
+    double dx_da1 = - link_1_length * sin(a_1) + dx_da2;
 
-    double dy_dpsi = S * cos(theta + phi + psi);
-    double dy_dphi = M * cos(theta + phi) + dy_dpsi;
-    double dy_dtht = L * cos(theta) + dy_dphi;
+    double dy_da4 = link_4_length * cos(a_1 + a_2 + a_3 + a_4);
+    double dy_da3 = link_3_length * cos(a_1 + a_2 + a_3) + dx_da4;
+    double dy_da2 = link_2_length * cos(a_1 + a_2) + dx_da3;
+    double dy_da1 = link_1_length * cos(a_1) + dx_da2;
 
     // Set up Jacobian J
-    double j[2][3];
-    j[0][0] = dx_dtht;
-    j[0][1] = dx_dphi;
-    j[0][2] = dx_dpsi;
-    j[1][0] = dy_dtht;
-    j[1][1] = dy_dphi;
-    j[1][2] = dy_dpsi;
+    double j[2][4];
+    j[0][0] = dx_da1;
+    j[0][1] = dx_da2;
+    j[0][2] = dx_da3;
+    j[0][3] = dx_da4;
+    j[1][0] = dy_da1;
+    j[1][1] = dy_da2;
+    j[1][2] = dy_da3;
+    j[1][3] = dy_da4;
 
     // Compute transposed Jacobian J^T
-    double jt[3][2];
-    for (int r = 0; r < 3; ++r)
+    double jt[4][2];
+    for (int r = 0; r < 4; ++r)
         for (int c = 0; c < 2; ++c)
             jt[r][c] = j[c][r];
 
@@ -204,16 +202,17 @@ void idle()
         for (int c = 0; c < 2; ++c)
         {
             jjt[r][c] = 0;
-            for (int k = 0; k < 3; ++k)
+            for (int k = 0; k < 4; ++k)
                 jjt[r][c] += j[r][k] * jt[k][c];
         }
 
     // Set angle increments with old values in case there is a singularity
-    double deltaTheta = oldDTheta;
-    double deltaPhi = oldDPhi;
-    double deltaPsi = oldDPsi;
+    double deltaA1 = oldDA1;
+    double deltaA2 = oldDA2;
+    double deltaA3 = oldDA3;
+    double deltaA4 = oldDA4;
 
-    // Computer determinant
+    // Compute determinant
     double det = jjt[0][0] * jjt[1][1] - jjt[0][1] * jjt[1][0];
     if (det == 0)
     {
@@ -229,8 +228,8 @@ void idle()
         jjti[1][1] =   jjt[0][0] / det;
 
         // Pseudoinverse = transpose * inverse
-        double psi[3][2];
-        for (int r = 0; r < 3; ++r)
+        double psi[4][2];
+        for (int r = 0; r < 4; ++r)
             for (int c = 0; c < 2; ++c)
             {
                 psi[r][c] = 0;
@@ -239,25 +238,30 @@ void idle()
             }
 
         // Obtain angle increments from pseudo-inverse
-        deltaTheta = psi[0][0] * deltaX + psi[0][1] * deltaY;
-        deltaPhi =   psi[1][0] * deltaX + psi[1][1] * deltaY;
-        deltaPsi =   psi[2][0] * deltaX + psi[2][1] * deltaY;
+        deltaA1 = psi[0][0] * deltaX + psi[0][1] * deltaY;
+        deltaA2 =   psi[1][0] * deltaX + psi[1][1] * deltaY;
+        deltaA3 =   psi[2][0] * deltaX + psi[2][1] * deltaY;
+        deltaA4 =   psi[3][0] * deltaX + psi[3][1] * deltaY;
 
         // Save increments in case we need them
-        oldDTheta = deltaTheta;
-        oldDPhi = deltaPhi;
-        oldDPsi = deltaPsi;
+        oldDA1 = deltaA1;
+        oldDA2 = deltaA2;
+        oldDA3 = deltaA3;
+        oldDA4 = deltaA4;
     }
 
     // Update angles and arm positions
-    theta += deltaTheta;
-    upper->setRot(theta);
+    a_1 += deltaA1;
+    link_1->setRot(a_1);
 
-    phi += deltaPhi;
-    middle->setRot(phi);
+    a_2 += deltaA2;
+    link_2->setRot(a_2);
 
-    psi += deltaPsi;
-    lower->setRot(psi);
+    a_3 += deltaA3;
+    link_3->setRot(a_3);
+
+    a_4 += deltaA4;
+    link_4->setRot(a_4);
 
     glutPostRedisplay();
 }
@@ -266,19 +270,6 @@ void keyboard (unsigned char key, int x, int y)
 {
     switch (key)
     {
-        case '+':
-            step *= 1.1;
-            break;
-        case '-':
-            step /= 1.1;
-            break;
-        case 'f':
-            glutFullScreen();
-            break;
-        case 't':
-            chooseTarget();
-            break;
-        case 'q':
         case 27:
             exit(0);
             break;
@@ -298,19 +289,12 @@ void reshape (int w, int h)
 
 int main(int argc, char *argv[])
 {
-    cout <<
-         "Pseudoinverse demonstration\n\n"
-         " +   faster\n"
-         " -   slower\n"
-         " t   choose new target\n"
-         " f   full screen\n"
-         " q   quit\n"
-         "ESC  quiit\n";
+    cout << "COMP 376 Assignment 2 Problem 2 \n" << "ESC Quit";
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
     glutInitWindowSize(windowWidth, windowHeight);
-    glutInitWindowPosition(300, 300);
-    glutCreateWindow("Inverse Kinematics using the Pseudoinverse");
+    glutInitWindowPosition(0, 0);
+    glutCreateWindow("COMP 376 Assignment 2 Problem 2");
     glutDisplayFunc(display);
     glutKeyboardFunc(keyboard);
     glutReshapeFunc(reshape);
